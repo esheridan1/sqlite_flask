67d51acfa55b1bdfd5c4f10857dbbdd7dc571555
esheridan1 <esheridan1@yahoo.com>
2024-09-04 15:51:17 +0200

    initial commit

--- hello.py
+++ hello.py
@@ -14,7 +14,7 @@ def index():
 	first_name = "John"
 	return render_template("index.html", first_name=first_name)
 
-# localhost:5000/user/John
+# localhost:5000/user/john
 @app.route("/user/<name>")
 
 def user(name):
