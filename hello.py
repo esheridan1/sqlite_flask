f047f0391f30b0dbf1b428a26bfc2e44ad00913a
esheridan1 <esheridan1@yahoo.com>
2024-09-04 15:17:05 +0200

    initial commit

---
+++ hello.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template
+
+
+#Create a Flask Instance
+app = Flask(__name__)
+
+#Create a route decorator
+@app.route("/")
+
+#def index():
+#	return "<h1>Hello World!<h1>"
+
+def index():
+	first_name = "John"
+	return render_template("index.html", first_name=first_name)
+
+# localhost:5000/user/John
+@app.route("/user/<name>")
+
+def user(name):
+	return render_template("user.html", user_name=name)
